a08172ab95f40e6d2b464438d4102c2e0d5e16c7
GOKUL <gokulabg123@gmail.com>
2026-08-21 11:34:07 +0530

    Time: 7 ms (99.08%), Space: 49.2 MB (43.08%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
+
+            // Overlap
+            if (currentStart <= end) {
+                end = Math.max(end, currentEnd);
+            }
+
+            // No overlap
+            else {
+                result.add(new int[]{start, end});
+
+                start = currentStart;
+                end = currentEnd;
+            }
+        }
+
+        
+        result.add(new int[]{start, end});
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
